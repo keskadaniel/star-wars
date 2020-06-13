b22c00a05a55405d4030356afe088ed48c8b3f7e
dkeska <keskadaniel@gmail.com>
2020-06-13 12:41:46 +0200

    initial commit

---
+++ src/test/java/pl/keskadaniel/starwars/StarWarsApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.keskadaniel.starwars;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarWarsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
