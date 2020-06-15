8248ee84997a75be6a095bf79deb194e35cb9991
dkeska <keskadaniel@gmail.com>
2020-06-15 14:52:26 +0200

    clean starship code

--- src/main/java/pl/keskadaniel/starwars/model/api/StarshipDto.java
+++ src/main/java/pl/keskadaniel/starwars/model/api/StarshipDto.java
@@ -1,27 +1,54 @@
 package pl.keskadaniel.starwars.model.api;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
+import pl.keskadaniel.starwars.model.base.SWstarship;
 
 import java.util.List;
 
 @Getter
 @Setter
-public class StarshipDto {
+@NoArgsConstructor
+public class StarshipDto extends SWstarship {
 
-    private String name;
-    private String model;
-    private String manufacturer;
-    private String cost_in_credits;
-    private String length;
-    private String max_atmosphering_speed;
-    private String crew;
-    private String passengers;
-    private String cargo_capacity;
-    private String consumables;
-    private String hyperdrive_rating;
-    private String MGLT;
-    private String starship_class;
+    @Override
+    @JsonProperty("cost_in_credits")
+    public String getCostInCredits() {
+        return super.getCostInCredits();
+    }
+
+    @Override
+    @JsonProperty("max_atmosphering_speed")
+    public String getMaxAtmosphericSpeed() {
+        return super.getMaxAtmosphericSpeed();
+    }
+
+    @Override
+    @JsonProperty("cargo_capacity")
+    public String getCargoCapacity() {
+        return super.getCargoCapacity();
+    }
+
+    @Override
+    @JsonProperty("hyperdrive_rating")
+    public String getHyperdriveRating() {
+        return super.getHyperdriveRating();
+    }
+
+    @Override
+    @JsonProperty("MGLT")
+    public String getMglt() {
+        return super.getMglt();
+    }
+
+    @Override
+    @JsonProperty("starship_class")
+    public String getStarshipClass() {
+        return super.getStarshipClass();
+    }
+
     private List<String> pilots;
     private List<String> films;
     private String created;
--- src/main/java/pl/keskadaniel/starwars/model/custom/CustomStarshipDto.java
+++ src/main/java/pl/keskadaniel/starwars/model/custom/CustomStarshipDto.java
@@ -2,23 +2,31 @@ package pl.keskadaniel.starwars.model.custom;
 
 import lombok.Builder;
 import lombok.Getter;
+import pl.keskadaniel.starwars.model.base.SWstarship;
 
-@Getter
-@Builder
-public class CustomStarshipDto {
 
-    private String name;
-    private String model;
-    private String manufacturer;
-    private String costInCredits;
-    private String length;
-    private String maxAtmosphericSpeed;
-    private String crew;
-    private String passengers;
-    private String cargoCapacity;
-    private String consumables;
-    private String hyperdriveRating;
-    private String mglt;
-    private String starshipClass;
+public class CustomStarshipDto extends SWstarship {
 
+//    private String name;
+//    private String model;
+//    private String manufacturer;
+//    private String costInCredits;
+//    private String length;
+//    private String maxAtmosphericSpeed;
+//    private String crew;
+//    private String passengers;
+//    private String cargoCapacity;
+//    private String consumables;
+//    private String hyperdriveRating;
+//    private String mglt;
+//    private String starshipClass;
+
+    @Builder
+    public CustomStarshipDto(String name, String model, String manufacturer, String costInCredits,
+                             String length, String maxAtmosphericSpeed, String crew, String passengers,
+                             String cargoCapacity, String consumables, String hyperdriveRating,
+                             String mglt, String starshipClass) {
+        super(name, model, manufacturer, costInCredits, length, maxAtmosphericSpeed, crew, passengers,
+                cargoCapacity, consumables, hyperdriveRating, mglt, starshipClass);
+    }
 }
