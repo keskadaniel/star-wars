1bc12580e9793ca3d365d5b426e7838a91020a6a
dkeska <keskadaniel@gmail.com>
2020-06-15 20:49:13 +0200

    add swagger config

--- src/test/java/pl/keskadaniel/starwars/StarWarsApplicationTests.java
+++ src/test/java/pl/keskadaniel/starwars/StarWarsApplicationTests.java
@@ -2,6 +2,7 @@ package pl.keskadaniel.starwars;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootTest
 class StarWarsApplicationTests {
